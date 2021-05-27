cfd7087b78aad86ab7ee25226620c500e9ea82af
DVE <celine.dv@hotmail.fr>
2021-05-27 19:15:48 +0200

    installation de vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Groupomania logo" src="./assets/logo.svg">
+  <Groupomania msg="Bienvenue sur notre tout nouveau réseau social d'entreprise !"/>
+</template>
+
+<script>
+import Groupomania from './components/groupomania.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Groupomania
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
