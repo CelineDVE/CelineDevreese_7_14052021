275140032baba049c79593a77a940e0298b52c09
DVE <celine.dv@hotmail.fr>
2021-05-27 23:24:17 +0200

    frontend init

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Groupomania logo" src="../assets/images/logo.svg">
+    <Groupomania msg="Bienvenue sur notre nouveau réseau social !"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Groupomania from '@/components/Home.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Groupomania
+  }
+}
+</script>
